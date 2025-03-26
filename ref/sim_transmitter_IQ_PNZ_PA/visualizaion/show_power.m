6e4ae008ae59318f9df8f09d8bb96856b92cd881
ChangShuo <changshuo@bupt.edu.cn>
2025-03-26 20:00:23 +0800

    reorganize the project folder

--- ref/sim_transmitter_IQ_PNZ_PA/visualizaion/show_power.m
+++
@@ -1,4 +0,0 @@
-function SignalMeanPower = show_power(dataname, data)
-    SignalMeanPower = mean(abs(data).^2);
-    fprintf([dataname, ' signal power is = %.3f\n'],SignalMeanPower);
-end
--- ref/sim_transmitter_IQ_PNZ_PA/visualizaion/show_snr.m
+++
@@ -1,8 +0,0 @@
-% ÒÑÓÐsnrº¯Êý
-function show_snr(signal, noise)
-    SNR = 10*log10(mean(abs(signal.^2))./mean(abs(noise.^2)));%dB
-    % SNR2 = 10*log10(power(rms(rxPA),2)/power(rms(noise),2))%dB
-    % SNR3 = 10*log10(sum(abs(rxPA).^2)./sum(abs(noise).^2))
-    % SNR4 = snr(rxPA,noise)
-    disp('SNR is ',SNR);
-end
